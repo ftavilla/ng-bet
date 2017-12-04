e30292bd5c549bfc8fd8a20b0c74c20e20469931
Fabrice Tavilla <fabrice.tavilla@gmail.com>
2017-12-04 21:20:54 +0100

    app reducers added

--- src/app/store/reducers/app.reducer.ts
+++ src/app/store/reducers/app.reducer.ts
@@ -1,3 +1,7 @@
-/**
- * Created by fabricetavilla on 03/12/2017.
- */
+import {ActionReducerMap} from "@ngrx/store";
+import {ngBetReducer} from "./ng-bet.reducer";
+import {AppState} from "../states/app.state";
+
+export const reducers: ActionReducerMap<AppState> = {
+  ngBet: ngBetReducer
+};
